452946a1dfe3ad485b3bbe995838a073fd6003e1
MariaInGithub <velascosheen06@gmail.com>
2025-12-27 15:03:30 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// ⚠️ IMPORTANT: Replace with your valid Facebook Access Token
+// Get it from: https://developers.facebook.com/tools/explorer
+// Empty token will show 401 (Unauthorized) error
+export const ACCESS_TOKEN = "EAAQgnU0gvkkBQSj7UueDEXZBYxKy7oREx8ZCvb4GWoK4RHB4gtCXIuL0ZAzmbvarpq8aGKrhFpps6v7WwEEYkMH7nc6cQFicEDgukx89Nx78Dkf5HquCscJyYvH334VvkQBQQskdAK1ZAK02D4yrXYdR8JIk2seFRvTZAKaD2pl9hZAamiPCewnsgYUAqek0xl58it0SZCMZBDV6uuAPmZBqkBtZBoRRoMefZBWXCTtDnrqarBHXsbdn8AmZB5fi6jwxtFqzxt2OamrnqM9saSN1Fs4Hvp6bZCj2BZAuc9r1N92bLjEhq45yagPQOxi0gLYedAsnZCpvZC0Jo6AOaUnQ";
+export const BASE_URL = "https://graph.facebook.com/v18.0";
